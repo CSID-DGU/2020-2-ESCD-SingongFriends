3f9a5518b37f317f479b6f990ce45e677b0804be
4whomtbts <4whomtbts@gmail.com>
2020-11-03 00:59:10 +0900

    test server

---
+++ sever/singongfriends/src/models/todo.model.ts
@@ -0,0 +1,33 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Todo extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: false,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'boolean',
+  })
+  isComplete?: boolean;
+
+
+  constructor(data?: Partial<Todo>) {
+    super(data);
+  }
+}
+
+export interface TodoRelations {
+  // describe navigational properties here
+}
+
+export type TodoWithRelations = Todo & TodoRelations;
