5e7b198ab42a4ca3d369b5f3344fe616dd99d30b
staris <sungdoolim@naver.com>
2020-11-22 18:57:44 +0900

    expense list

--- pages/staris/page1/page1.js
+++ pages/staris/page1/page1.js
@@ -6,6 +6,9 @@ Page({
    * Page initial data
    */
   data: {
+    studentId:1,
+    semester:[20192,20201,20202],
+    forindex:[0,1,2],
     ar:['안','녕','하','세','요'],
     arr:['a','b','c'],
     object: {
@@ -32,11 +35,52 @@ Page({
     a:3,
     b:2,
     arobj:[],
+    result:[]
     
     
   },
   
 
+  ReqRes:function(url,i){
+    var that=this
+    wx.request({// 'http://staris.freehongs.net/web/androidtest.do'
+     //
+     url :url,
+     method: 'GET',
+      data:{},
+      success: function (res) {
+       // console.log(JSON.stringify(res.data))
+      // that.setData({curlist:res.data.sendData})
+        console.log(res.data);
+        //console.log(res.data.sendData[0].tuition)
+       // console.log(res.data[0].studentId);
+       that.data.arobj.push(res.data)
+       that.setData({
+         result:that.data.arobj,
+        //arobj:arobj
+         // arobj:JSON.stringify(res.data)
+        // arobj:res.data
+
+      //   studentExpenseId: res.data.studentExpenseId,
+      //  amountBeforeReduction: res.data.amountBeforeReduction,
+      //  reductionAmount: res.data.reductionAmount,
+      //  amountAfterReduction: res.data.amountAfterReduction,
+
+        // obj:{key1:res.data.sendData[0].f,key2:res.data.sendData[0].l,key3:'?'},
+           //arobj:[this.data.object,this.data.object2,this.data.object3,this.data.object4],
+           //arobj:res.data.sendData
+       })
+       //console.log(that.data)
+      },
+      fail: function(res){
+
+        },
+      complete: function(){
+
+      }
+    });
+    
+  },
   /**
    * Lifecycle function--Called when page load
    */
@@ -46,57 +90,35 @@ Page({
     // var a=this.data.a;
     //wx5be09fc9ea2bb7af
     const accountInfo = wx.getAccountInfoSync();
-    console.log(accountInfo) // 小程序 appId
+  //  console.log(accountInfo) // 小程序 appId
 
     wx.login({
       success (res) {
-       // console.log(res.code)
+      //  console.log(res.code)
+       // console.log(res)
       }
     })
     //052M8a000HQ6GK1Laf100aPXOU0M8a0D
     //0924da000XT6GK1l4l200kPNH044da0S
+    var url="http://119.28.235.170/students/student/"+this.data.studentId+"/student-expenses/semester/"+this.data.semester
+    for(var i=0;i<this.data.semester.length;i+=1){
+      url="http://119.28.235.170/students/student/"+this.data.studentId+"/student-expenses/semester/"+this.data.semester[i]
+      this.ReqRes(url,i)
+      console.log(i)
+    }
+
+    console.log(this.data.arobj.toString())
+
+
+
     this.setData({
+      result:this.data.arobj
       // sesid:app.globalData.sesid
      })
+     console.log(this.data.result)
    //  console.log("culistid : "+this.data.sesid);
    //http://119.28.235.170/students
-     var that=this
-     wx.request({//    get
-      //http://staris.freehongs.net/web/androidtest.do
-      url :'http://119.28.235.170/expenses',
-       method: 'GET',
-       data:{
-         // name: "pay",
-         // studentCode: "20121313",
-         // wechatToken: "helloworld"
-       },
-       success: function (res) {
-       //  console.log(JSON.stringify(res.data))
-       // that.setData({curlist:res.data.sendData})
-        // console.log(res.data[0].studentId);
-         //console.log(res.data.sendData[0].tuition)
-        // console.log(res.data[0].studentId);
-        that.setData({
-          // arobj:JSON.stringify(res.data)
-          arobj:res.data
-         // obj:{key1:res.data.sendData[0].f,key2:res.data.sendData[0].l,key3:'?'},
-     
-            //arobj:[this.data.object,this.data.object2,this.data.object3,this.data.object4],
-            //arobj:res.data.sendData
-           
-        })
-       // console.log(that.data)
-       },
-       fail: function(res){
-        that.setData({
-             arobj:res.data
-         })
-         },
-       complete: function(){
-         that.setData({ })
-       }
-     });
-     
+
 
   },
   go:function(){
