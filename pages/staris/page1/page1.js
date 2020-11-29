47c13ca419c71142805a3e53a0a3d16e5e398a6a
staris <sungdoolim@naver.com>
2020-11-29 20:09:35 +0900

    add , format

--- pages/staris/page1/page1.js
+++ pages/staris/page1/page1.js
@@ -7,9 +7,12 @@ Page({
    */
   data: {
     code:"",
+    lenresult:0,
+    count:0,
     studentId:1,
     openId:"",
     semester:[20192,20201,20202],
+    semester_view:["2019-2","2020-1","2020-2"],
     forindex:[0,1,2],
     ar:['안','녕','하','세','요'],
     arr:['a','b','c'],
@@ -73,8 +76,6 @@ Page({
      });
   },
   pay:function(url){
-   
-
   },
   ReqRes:function(url,i){
     var that=this
@@ -84,28 +85,60 @@ Page({
      method: 'GET',
       data:{},
       success: function (res) {
-       // console.log(JSON.stringify(res.data))
-      // that.setData({curlist:res.data.sendData})
         console.log(res.data);
-        //console.log(res.data.sendData[0].tuition)
-       // console.log(res.data[0].studentId);
-       that.data.arobj.push(res.data)
+        var stramount=res.data.amountBeforeReduction.toString();
+        var len=stramount.length;
+        var newstr="";
+        console.log(stramount[1]);
+        console.log(len);
+        switch(len){
+          case 4:
+            for(var k=0;k<len+1;k++){
+              if(k==1){
+                newstr+=",";
+
+              }
+              newstr+=stramount[k];
+            }
+            break;
+            case 5:
+              for(var k=0;k<len;k++){
+                if(k==2){
+                  newstr+=",";
+                }
+                newstr+=stramount[k];
+              }
+              break;
+              case 6:
+                for(var k=0;k<len;k++){
+                  if(k==3){
+                    newstr+=",";
+
+                  }
+                  newstr+=stramount[k];
+                }
+                break;
+                case 7:
+                  for(var k=0;k<len;k++){
+                    if(k==1||k==4){
+                      newstr+=",";
+
+                    }
+                    newstr+=stramount[k];
+                  }
+                  break;
+                  default:
+                    break;
+
+        }
+        res.data.amountBeforeReduction=newstr;
+        console.log(res.data.amountBeforeReduction+"g");
+       that.data.arobj.push(res.data);
+       //that.data.arobj.push(JSON.stringify(res.data));
+
        that.setData({
          result:that.data.arobj,
-        //arobj:arobj
-         // arobj:JSON.stringify(res.data)
-        // arobj:res.data
-
-      //   studentExpenseId: res.data.studentExpenseId,
-      //  amountBeforeReduction: res.data.amountBeforeReduction,
-      //  reductionAmount: res.data.reductionAmount,
-      //  amountAfterReduction: res.data.amountAfterReduction,
-
-        // obj:{key1:res.data.sendData[0].f,key2:res.data.sendData[0].l,key3:'?'},
-           //arobj:[this.data.object,this.data.object2,this.data.object3,this.data.object4],
-           //arobj:res.data.sendData
        })
-       //console.log(that.data)
       },
       fail: function(res){
 
@@ -121,58 +154,50 @@ Page({
    */
   onLoad: function (options) {
     var that=this;
-
     wx.login({
-      
       success (res) {
        console.log("jscode : "+res.code)
-
        that.setData({
          code:res.code
        })
-       // console.log(res)
- 
       }
-    
     })
-    //052M8a000HQ6GK1Laf100aPXOU0M8a0D
-    //0924da000XT6GK1l4l200kPNH044da0S
-    var url="http://119.28.235.170/students/student/"+this.data.studentId+"/student-expenses/semester/"+this.data.semester
-    for(var i=0;i<this.data.semester.length;i+=1){
+    var url="";
+    var len=this.data.semester.length;
+     for(var i=0;i<len;i+=1){
       url="http://119.28.235.170/students/student/"+this.data.studentId+"/student-expenses/semester/"+this.data.semester[i]
       this.ReqRes(url,i)
-      //console.log(i)
     }
-   // console.log(this.data.arobj.toString())
-//this.pay();
-
-
-
     this.setData({
-      result:this.data.arobj
-      // sesid:app.globalData.sesid
-     })
-     console.log(this.data.result)
-   //  console.log("culistid : "+this.data.sesid);
-   //http://119.28.235.170/students
+      result:this.data.arobj,
+      lenresult:this.data.count
+     });
+
+     var k=this.data.result;
+    // var k=JSON.stringify(res.data)
+
+     for(var i=0;i<len;i++){
+       console.log(k[i]);
+     }
 
 
+
+   
   },
   go:function(){
 console.log("늉");
   },
-  //Date.now().toString()
   gopage2:function(){
     wx.navigateTo({
       url: '../page2/page2',
     })
-
   },
   /**
    * Lifecycle function--Called when page is initially rendered
    */
   onReady: function () {
     //this.getOpenid();
+
   },
 
   /**
